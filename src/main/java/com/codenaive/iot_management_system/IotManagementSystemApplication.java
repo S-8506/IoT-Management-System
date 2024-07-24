67f9b3c4986810c8af7cd0e101275ff55afb8276
Santosh Kumar <S.8506.A@gmail.com>
2024-07-24 21:35:32 +0530

    initialize project

---
+++ src/main/java/com/codenaive/iot_management_system/IotManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.codenaive.iot_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotManagementSystemApplication.class, args);
+	}
+
+}
